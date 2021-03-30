46865534356c5da3520f4d68cb76b46a45976577
Paul Ajibola <ife.jibola@gmail.com>
2021-03-30 11:58:36 -0400

    Initial Commit

---
+++ PRG2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PRG2
+//
+//  Created by Ife Ajibola on 2021-03-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
